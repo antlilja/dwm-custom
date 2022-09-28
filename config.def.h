9c85e75b1ac324e74967f4ae362e9f64fbdf9cba
antlilja <liljaanton2001@gmail.com>
2022-09-28 19:26:58 +0200

    Changed st to alacritty and added more programs

--- config.def.h
+++ config.def.h
@@ -55,7 +55,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
+static const char *discordcmd[]  = { "discord", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
